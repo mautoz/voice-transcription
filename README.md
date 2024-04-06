96f1c0cdf953c053de7e24f9020d19eb1b80550d
Mauricio <ozaki@versatushpc.com.br>
2024-04-06 00:33:04 -0300

    Removing print, put comments and adding readme info

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Voice Transcription
+# C3PO Voice Transcription
 
-Do you want to transcribe voice messages on Telegram? This is a simple code to transcript voice messages sent to Telegram. I speak Brazilian Portuguese, but it's easy to change the language.
+> "I am fluent in over six million forms of communication."
+
+Do you want to transcribe voice messages on Telegram? This is a simple code to 
+transcript voice messages sent to Telegram. Despite the real C3PO skills, this
+script is made for Brazilian Portuguese, but it's easy to change the language.
+
+## Is it available?
+
+Yes! You can add in your Telegram group using @c3po_voice_transcription_bot.
+
+> "You watch your language!"
 
 ## Dependencies
 
@@ -20,5 +30,13 @@ $ rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-
 Install the FFmpeg and FFmpeg development packages.
 
 $ yum install ffmpeg ffmpeg-devel -y
+
+## Bugs?
+
+> "Help! I think I'm melting! This is all your fault!"
+
+Please, send me a message! Look my profile for any contacts!
+
+@2024
 
 
--- voice_transcription_bot.py
+++ voice_transcription_bot.py
@@ -1,25 +1,40 @@
+#!/usr/bin/env python3
+"""
+C3PO Voice Transcription
+
+This is a simple Telegram bot script to transcript voice messages.
+
+Install all dependencies and run the script!
+"""
 import os
-import telegram
 from tempfile import TemporaryDirectory
+import telegram
+
 import speech_recognition as sr
 from pydub import AudioSegment
 from telegram.ext import Updater, MessageHandler, Filters
+
 from dotenv import load_dotenv
 load_dotenv()
 
 # Replace YOUR_BOT_TOKEN with your Telegram bot token
-bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
+BOT = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
 
 def transcribe_audio(update, context):
+    """
+    The voice message is downloaded in a temporary folder
+    and the speech recognition do the transcription!
+    """
     with TemporaryDirectory() as temp_path:
         # Get voice file from Telegram message
-        file = context.bot.getFile(update.message.voice.file_id)
+        file = context.BOT.getFile(update.message.voice.file_id)
         file_name = file.file_path.split("/")[-1]
         file_path = os.path.join(temp_path, file_name)
         file.download(file_path)
+        
         # Check the audio format
         file_format = file_name.split(".")[-1]
-        print(file_format)
+        
         if file_format not in ["wav", "aiff", "aif", "aifc", "flac"]:
             sound = AudioSegment.from_file(file_path, format='ogg')            
             file_path = os.path.join(temp_path, file_name.split(".")[0] + ".wav")
@@ -33,15 +48,18 @@ def transcribe_audio(update, context):
             text = r.recognize_google(audio, language="pt-BR")
             update.message.reply_text(text)
         except sr.UnknownValueError:
-            update.message.reply_text("It's not Portuguese! Let's try English!")
-            try:
-                text = text = r.recognize_google(audio, language="en-US")
-            except sr.UnknownValueError:
-                update.message.reply_text("I am fluent in over six million forms of communication, but I couldn't understand you! Can you repeat?")
+            update.message.reply_text(
+                "I am fluent in over six million forms of communication, "
+                "but I couldn't understand you! Can you repeat?"
+            )
         except sr.RequestError as err:
             update.message.reply_text(f"An error occurred: {str(err)}")
 
 def main():
+    """
+    Be careful with your Telegram Token, do not put in your code
+    use environment variable.
+    """
     updater = Updater(token=os.getenv('TELEGRAM_TOKEN'), use_context=True)
     message_handler = MessageHandler(Filters.voice, transcribe_audio)
     updater.dispatcher.add_handler(message_handler)
@@ -50,3 +68,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
